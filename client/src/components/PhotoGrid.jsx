1cfe737aaa3c3662bd5ad779770b837510811b03
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-05-25 14:50:13 -0700

    designed photo grid

--- client/src/components/PhotoGrid.jsx
+++ client/src/components/PhotoGrid.jsx
@@ -7,23 +7,24 @@ class PhotoGrid extends React.Component {
 
     }
   }
-
+  onClick() {
+    // console.log(this.props.photos);
+  }
   render() {
     return (
-      <div className="photo-grid-container">
-        <h2 className="photos-grid-header">{this.props.photos.length} Photos</h2>
-        <div className="photo-grid">
-          <img className="photo photo-A" key={this.props.photos[0].id} src={this.props.photos[0].url} style={{ width: 142, height: 142 }} onClick={() => this.clickPhoto(this.props.photos[0].id)} />
-          {/* <img className="photo photoB" key={this.props.photos[1].id} src={this.props.photos[1].url} style={{ width: 142, height: 142 }} onClick={() => this.clickPhoto(this.props.photos[1].id)} />
-          <img className="photo photoC" key={this.props.photos[2].id} src={this.props.photos[2].url} style={{ width: 288, height: 288 }} onClick={() => this.clickPhoto(this.props.photos[2].id)} />
-          <img className="photo photoD" key={this.props.photos[3].id} src={this.props.photos[3].url} style={{ width: 142, height: 142 }} onClick={() => this.clickPhoto(this.props.photos[3].id)} />
-          <img className="photo photoE" key={this.props.photos[4].id} src={this.props.photos[4].url} style={{ width: 142, height: 142 }} onClick={() => this.clickPhoto(this.props.photos[4].id)} />
-          <img className="photo photoF" key={this.props.photos[5].id} src={this.props.photos[5].url} style={{ width: 288, height: 288 }} onClick={() => this.clickPhoto(this.props.photos[5].id)} />
-          <img className="photo photoG" key={this.props.photos[6].id} src={this.props.photos[6].url} style={{ width: 142, height: 142 }} onClick={() => this.clickPhoto(this.props.photos[6].id)} />
-          <img className="photo photoH" key={this.props.photos[7].id} src={this.props.photos[7].url} style={{ width: 142, height: 142 }} onClick={() => this.clickPhoto(this.props.photos[7].id)} />
-          <img className="photo photoI" key={this.props.photos[8].id} src={this.props.photos[8].url} style={{ width: 288, height: 288 }} onClick={() => this.clickPhoto(this.props.photos[8].id)} />
-          <img className="photo photoJ" key={this.props.photos[9].id} src={this.props.photos[9].url} style={{ width: 142, height: 142 }} onClick={() => this.clickPhoto(this.props.photos[9].id)} />
-          <img className="photo photoK" key={this.props.photos[10].id} src={this.props.photos[10].url} style={{ width: 142, height: 142 }} onClick={() => this.clickPhoto(this.props.photos[10].id)} /> */}
+      <div className="photo-grid-container" onClick={() => console.log(this.props.photos)}>
+        <div className="photo-grid-left">
+          <img className="photo-grid-list-img" src={this.props.photos[0].url} />
+          <img className="photo-grid-list-img-center" src={this.props.photos[1].url} />
+          <img className="photo-grid-list-img" src={this.props.photos[2].url} />
+        </div>
+        <div className="photo-grid-right">
+          <img className="photo-grid-list-img" src={this.props.photos[3].url} />
+          <img className="photo-grid-list-img" src={this.props.photos[4].url} />
+          <img className="photo-grid-list-img" src={this.props.photos[5].url} />
+          <img className="photo-grid-list-img" src={this.props.photos[6].url} />
+          <img className="photo-grid-list-img" src={this.props.photos[7].url} />
+          <img className="photo-grid-list-img" src={this.props.photos[8].url} />
         </div>
       </div>
     );
@@ -31,3 +32,18 @@ class PhotoGrid extends React.Component {
 }
 
 export default PhotoGrid;
+
+// {
+//   this.props.photos.map(photo => {
+//     if (photo.id > 3 && photo.id < 10) {
+//       (
+//         <div>
+//           <img className="photo-grid-list-img" src={photo.url} />
+//         </div>
+//       )
+//     }
+//   }
+//   )
+// }
+
+{/* <img className="photo photo-A" key={this.props.photos[0].id} src={this.props.photos[0].url} style={{ width: 142, height: 142 }} onClick={() => this.clickPhoto(this.props.photos[0].id)} /> */ }
--- client/src/components/SaveRestaurant.jsx
+++ client/src/components/SaveRestaurant.jsx
@@ -4,16 +4,41 @@ class SaveRestaurant extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      saveRestaurant: false,
+    }
+    this.clickSave = this.clickSave.bind(this);
+  }
+  clickSave() {
+    if (this.state.saveRestaurant) {
+      this.setState({ saveRestaurant: false })
+    } else if (!this.state.saveRestaurant) {
+      this.setState({ saveRestaurant: true })
     }
   }
   render() {
-    return (
-      <button className="btn-save-restaurant">
-        <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1"><g id="icon/ic_bookmark" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"><path d="M7,2.99987873 L17,2.99987873 C18.1045695,2.99987873 19,3.89530923 19,4.99987873 L19,20.6055572 C19,20.8816996 18.7761424,21.1055572 18.5,21.1055572 C18.421402,21.1055572 18.343911,21.0870278 18.273815,21.0514724 L12,17.8691561 L5.72618504,21.0514724 C5.4799129,21.1763909 5.17900339,21.0780144 5.05408484,20.8317423 C5.01852946,20.7616462 5,20.6841552 5,20.6055572 L5,4.99987873 C5,3.89530923 5.8954305,2.99987873 7,2.99987873 Z M7,5 L7,18.3374734 L12,15.7665154 L17,18.3374734 L17,5 L7,5 Z" id="Combined-Shape" fill="#333333"></path></g></svg>
-        <span>Save this restaurant</span>
-      </button>
-    )
+    if (!this.state.saveRestaurant) {
+      return (
+        <div className="save-restaurant-container" onClick={() => this.clickSave()}>
+          <button className="save-restaurant-btn">
+            <div className="save-restaurant-text-container">
+              <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1"><g id="icon/ic_bookmark" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"><path d="M7,2.99987873 L17,2.99987873 C18.1045695,2.99987873 19,3.89530923 19,4.99987873 L19,20.6055572 C19,20.8816996 18.7761424,21.1055572 18.5,21.1055572 C18.421402,21.1055572 18.343911,21.0870278 18.273815,21.0514724 L12,17.8691561 L5.72618504,21.0514724 C5.4799129,21.1763909 5.17900339,21.0780144 5.05408484,20.8317423 C5.01852946,20.7616462 5,20.6841552 5,20.6055572 L5,4.99987873 C5,3.89530923 5.8954305,2.99987873 7,2.99987873 Z M7,5 L7,18.3374734 L12,15.7665154 L17,18.3374734 L17,5 L7,5 Z" id="Combined-Shape" fill="#333333"></path></g></svg>
+              <span className="save-restaurant-text">Save this restaurant</span>
+            </div>
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="save-restaurant-container" onClick={() => this.clickSave()}>
+          <button className="save-restaurant-btn">
+            <div className="save-restaurant-text-container">
+              <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1"><g id="icon/ic_bookmarked" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"><g id="icon/ic_bookmark" transform="translate(5.000000, 3.000000)" fill="#DA3743"><path d="M2,-0.000121269196 L12,-0.000121269196 C13.1045695,-0.000121269196 14,0.895309231 14,1.99987873 L14,17.6055572 C14,17.8816996 13.7761424,18.1055572 13.5,18.1055572 C13.421402,18.1055572 13.343911,18.0870278 13.273815,18.0514724 L7,14.8691561 L0.726185039,18.0514724 C0.479912898,18.1763909 0.17900339,18.0780144 0.054084842,17.8317423 C0.0185294562,17.7616462 2.78518304e-15,17.6841552 2.77555756e-15,17.6055572 L0,1.99987873 C-6.25798147e-16,0.895309231 0.8954305,-0.000121269196 2,-0.000121269196 Z" id="Combined-Shape"></path></g></g></svg>
+              <span className="save-restaurant-text">Restaurant saved!</span>
+            </div>
+          </button>
+        </div>
+      );
+    }
   }
 }
 
